b3e4b2b33ac518e48b0a520e0e18947f7f71cec1
Ryan Powell <try_try_again@protonmail.com>
2018-01-26 19:38:58 -0800

    initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("I'll touch whatever the fuck I want thank you very much :D")
